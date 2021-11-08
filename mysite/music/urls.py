03043e153327d7bc58950b3b80ccca745ff9f7e7
Rayyan Shikoh <75220358+rayyanshikoh@users.noreply.github.com>
2021-11-08 18:29:36 +0500

    css, headers, login page, and file uploads

--- mysite/music/urls.py
+++ mysite/music/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 app_name = "music"   
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
+    path("home/", views.homepage, name="homepage"),
     path("register/", views.register_page, name="register"),
     path("login/", views.login_page, name="login"),
-    path("pricing/", views.pricing, name='pricing')
+    path("logout/", views.logoutuser, name="logout"),
+    path("pricing/", views.pricing, name='pricing'),
+    path("upload/", views.simple_upload, name="upload")
 ]
--- mysite/music/views.py
+++ mysite/music/views.py
@@ -1,27 +1,84 @@
 from django.shortcuts import  render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.http import HttpResponse
 from .forms import CreateUserForm
+from django.contrib.auth.decorators import login_required
+from django.core.files.storage import FileSystemStorage
+import shutil
+from os import path
+import zipfile
 
-def homepage(request):
-    return render(request, 'music/home.html', {})
+def extractor(filename, url):
+    source_path = url
+    with zipfile.ZipFile(source_path, 'r') as zip_ref:
+        zip_ref.extractall(f"./media/music/{filename}")
+
+
 
 def register_page(request):
-    form = CreateUserForm()
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-        
-    context = {'form':form}
-    return render(request, 'music/register.html', context)
+    if request.user.is_authenticated:
+        return redirect('music:homepage')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, "Account was created for " + user)
+                return redirect('music:login')
+        context = {'form':form}
+        return render(request, 'music/register.html', context)
+
 
 def login_page(request):
-    context = {}
-    return render(request, 'music/login.html', context)
+    if request.user.is_authenticated:
+        return redirect('music:homepage')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            print('authenticating')
+            user = authenticate(request=request, username=username, password=password)
+            print('authenticated')
+            if user is not None:
+                print('logging in')
+                login(request, user)
+                print('logged in')
+                return redirect('music:homepage')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
+                # return render(request, 'music/login.html', context)
+        context = {}
+        return render(request, 'music/login.html', context)
 
+def logoutuser(request):
+    logout(request)
+    return redirect('music:login')
+
+@login_required(login_url='music:login')
+def homepage(request):
+    context = {}
+    return render(request, 'music/home.html', context)
+
+@login_required(login_url='music:login')
 def pricing(request):
     context = {}
     return render(request, 'music/pricing.html', context)
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        print(uploaded_file_url)
+        new_url = f".{str(uploaded_file_url)}"
+        extractor(filename=myfile.name, url=new_url)
+        # return render(request, 'music/upload.html', {
+        #     'uploaded_file_url': uploaded_file_url
+        # })
+        return redirect('music:homepage')
+    return render(request, 'music/upload.html')
