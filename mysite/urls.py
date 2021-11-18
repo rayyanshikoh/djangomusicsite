4bd09293e01e9baa1172ef7be98cdc354cb63048
Rayyan Shikoh <75220358+rayyanshikoh@users.noreply.github.com>
2021-11-18 19:40:58 +0500

    edit preload data and attempted fix on static files

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,8 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('music/', include('music.urls')),
     # re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-]
+#     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+# ]
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
