a41316e7c5aa63a64de294107fd43280c695b660
Rayyan Shikoh <75220358+rayyanshikoh@users.noreply.github.com>
2021-11-18 15:36:08 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#cqsxo6npt%)oi0o@v+09z9p)t8z!)s#%rs9xk8=1d*p=0tonv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'rayyanify.herokuapp.com']
 
