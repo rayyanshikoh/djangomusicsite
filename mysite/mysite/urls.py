bc3ff1abd25ce0b7b5ed2a8ebed4380d77913fbb
Rayyan Shikoh <75220358+rayyanshikoh@users.noreply.github.com>
2021-11-18 15:15:10 +0500

    prep for heroku deployment

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('music/', include('music.urls'))
+    path('music/', include('music.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += staticfiles_urlpatterns()
